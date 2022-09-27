45271ae0df26478cba01211ea5cd19e20c39cc82
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2022-09-27 15:56:31 +0530

    Update TestFileTxt.m

--- buildutil/TestFileTxt.m
+++ buildutil/TestFileTxt.m
@@ -1,5 +1,4 @@
 function TestFileTxt()
-
 % get the current directory and all the files from code folder
 rootDir = pwd();
 FunctionFileInfo = dir(fullfile(rootDir,"code","*.mlx"));
@@ -9,17 +8,20 @@ allFiles= vertcat(FunctionFileInfo,FunctionFileInfo1);
 
 % generate Test_parameters.txt file based on number of function files in
 % the code folder
-parallel.internal.pool.isPoolWorker = 0;
-testFileInfo = fullfile(rootDir,"test","Test_parameters.txt");
-fid = fopen(testFileInfo,'w');
+testFileInfo = fullfile(rootDir,"test","Test_parameters.json");
+fid = fopen(testFileInfo,"w");
 for i=1:length(allFiles)
-    a = string(allFiles(i).name);
-    string_new = strsplit(a,'.');
-    disp(class(string_new(1)));
-    %fprintf(fid, '%s:\n',string_new(1));
-    fprintf(fid, 'Input parameter:\n');
-    fprintf(fid, 'Expected Output:\n');
+    ParamInfo = struct;
+    ParamInfo.name = allFiles.name;
+    ParamInfo.Input_parameter = "{2,3}";
+    ParamInfo.Expected_output = "5";
+    ParamJSON = jsonencode(ParamInfo);
+    try
+        fwrite(fid,ParamJSON);
+    catch e
+        fclose(fid);
+        rethrow e
+    end
 end
 fclose(fid);
-
 end

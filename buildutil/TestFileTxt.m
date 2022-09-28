0943b2512b3f28fa70f53645521ea015a96e1bf2
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2022-09-28 10:53:49 +0530

    Update TestFileTxt.m

--- buildutil/TestFileTxt.m
+++ buildutil/TestFileTxt.m
@@ -1,28 +1,29 @@
 function TestFileTxt()
 
-% get the current directory and all the files from code folder
-rootDir = pwd();
-FunctionFileInfo = dir(fullfile(rootDir,"code","*.mlx"));
-FunctionFileInfo1 = dir(fullfile(rootDir,"code","*.m"));
-allFiles= vertcat(FunctionFileInfo,FunctionFileInfo1);
-
-
-% generate Test_parameters.txt file based on number of function files in
-% the code folder
-testFileInfo = fullfile("test","Test_parameters.json");
-fid = fopen(testFileInfo,"w");
-ParamInfo = struct;
-for i=1:length(allFiles)
-    ParamInfo(i).name = allFiles(i).name;
-    ParamInfo(i).Input_parameter = "{2,3}";
-    ParamInfo(i).Expected_output = "5";
-end
-badgeJSON = jsonencode(ParamInfo);
-try
-    fwrite(fid,badgeJSON);
-catch e
+    % get the current directory and all the files from code folder
+    rootDir = pwd();
+    FunctionFileInfo = dir(fullfile(rootDir,"code","*.mlx"));
+    FunctionFileInfo1 = dir(fullfile(rootDir,"code","*.m"));
+    allFiles= vertcat(FunctionFileInfo,FunctionFileInfo1);
+    
+    
+    % generate Test_parameters.txt file based on number of function files in
+    % the code folder
+    testFileInfo = fullfile(rootDir,"test","Test_parameters.json");
+    ParamInfo = struct;
+    for i=1:length(allFiles)
+        ParamInfo(i).name = allFiles(i).name;
+        ParamInfo(i).Input_parameter = "{2,3}";
+        ParamInfo(i).Expected_output = "5";
+    end
+    badgeJSON = jsonencode(ParamInfo);
+    disp(badgeJSON)
+    fid = fopen(testFileInfo,"w");
+    try
+        fwrite(fid,badgeJSON);
+    catch e
+        fclose(fid);
+        rethrow e
+    end
     fclose(fid);
-    rethrow e
-end
-fclose(fid);
 end

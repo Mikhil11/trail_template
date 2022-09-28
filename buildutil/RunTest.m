ecd62aa59a945369c44c4b0dc4dcfc3f11d0af6d
Mikhil Shetty <111747532+Mikhil11@users.noreply.github.com>
2022-09-28 15:49:48 +0530

    Update RunTest.m

--- buildutil/RunTest.m
+++ buildutil/RunTest.m
@@ -1,12 +1,15 @@
 import matlab.unittest.parameters.Parameter
 import matlab.unittest.TestSuite
 rootDir = pwd();
+testFileInfo1 = fullfile(rootDir,"tests","Unittest_results.txt");
 testFileInfo = dir(fullfile(rootDir,"code"));
 addpath(testFileInfo.folder);
 [arg1,arg2,arg3] = Getparameters();
+fid = fopen(testFileInfo,"w");
 for i=1:length(arg1)
     param = Parameter.fromData("fname",{arg1(i)},"ainput",{arg2(i)},"exp_output",{arg3(i)});
     suite2 = TestSuite.fromClass(?SolverTest,"ExternalParameters",param);
     results = suite2.run;
-    disp(results);
+    fprintf(fid,"%s:\n",string(results));
 end
+fclose(fid);
